b991bbea6e6391f9c0b9a967bc6b1fde92c4bffb
Daniel Murguia <14241510+DMurguia08@users.noreply.github.com>
2022-04-23 23:56:23 -0700

    Progressive Web Application

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -8,7 +9,13 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Output Management',
+            title: 'Progressive Web Application',
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // These options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
         }),
     ],
     output: {
