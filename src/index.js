ccaed2106a032179fc56cee3ade7f47e2619e5b1
Daniel Murguia <14241510+DMurguia08@users.noreply.github.com>
2022-04-23 22:28:58 -0700

    Lazy Loading Examples

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-async function getComponent() {
-    const { default: _ } = await import('lodash')
-    const element = document.createElement('div')
+import { join } from 'lodash'
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+function getComponent() {
+    const element = document.createElement('div')
+    const button = document.createElement('button')
+    const br = document.createElement('br')
+
+    button.innerHTML = 'Click me and look at the console!'
+    element.innerHTML = join(['Hello', 'webpack'], ' ')
+    element.append(br, button)
+
+    // Note that because the network request is involved, some indication
+    // of loading would need to be shown in the production-level site/app.
+    button.onclick = () => import(/* webpackChunkName: "print" */ './print.js').then(module => {
+        const print = module.default
+
+        print()
+    })
 
     return element
 }
 
-getComponent().then(component => {
-    document.body.appendChild(component)
-})
+document.body.appendChild(getComponent())
