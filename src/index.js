17fdad604eaf75f993e624c67b7d6305cef073ba
Daniel Murguia <14241510+DMurguia08@users.noreply.github.com>
2022-04-23 23:13:25 -0700

    Loading Polyfills

--- src/index.js
+++ src/index.js
@@ -10,3 +10,13 @@ function component() {
 }
 
 document.body.appendChild(component())
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+        console.log(
+            'We retrieved some data! AND we\'re confident it will work on a variety of browser distributions.'
+        )
+        console.log(json)
+    })
+    .catch(err => console.error('Something went wrong when fetching this data: ', err))
