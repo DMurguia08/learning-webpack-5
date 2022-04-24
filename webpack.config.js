3042f113e340c74f88dfb217c964ef7764d5b10a
Daniel Murguia <14241510+DMurguia08@users.noreply.github.com>
2022-04-23 17:09:46 -0700

    Environment Variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+module.exports = (env) => {
+    //* The CLI command would be:
+    // npx webpack --env goal=local --env production --progress
+    //* User env.<YOUR_VARIABLE> here:
+    console.log('Goal: ', env.goal)
+    console.log('Production: ', env.production)
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+    }
 }
